745baf463270b745915c9aabcd498fdd8b329af3
Zong Han <zonghangoh@gmail.com>
2024-06-16 22:38:25 +0800

    fix dynamic import path typo

--- app/frontend/entrypoints/application.jsx
+++ app/frontend/entrypoints/application.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 
 createInertiaApp({
-    resolve: name => import(`./pages/${name}.jsx`),
+    resolve: name => import(`../pages/${name}.jsx`),
     setup({ el, App, props }) {
         const container = document.getElementById(el.id);
         const root = createRoot(container);
